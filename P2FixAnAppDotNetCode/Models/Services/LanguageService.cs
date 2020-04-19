cc42461ac7153e1d38964dfce59e11f45cc8e365
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2020-04-19 09:31:08 +0300

    LanguageServiceTests - SetCulture
    This change takes you through on how the SetCulture test was resolved

--- P2FixAnAppDotNetCode/Models/Services/LanguageService.cs
+++ P2FixAnAppDotNetCode/Models/Services/LanguageService.cs
@@ -25,7 +25,18 @@ namespace P2FixAnAppDotNetCode.Models.Services
             string culture = "";
             // TODO complete the code 
             // Default language is "en", french is "fr" and spanish is "es".
-            
+            switch (language)
+            {
+                case "French":
+                    culture = "fr";
+                    break;
+                case "Spanish":
+                    culture = "es";
+                    break;
+                default:
+                    culture = "en";
+                    break;
+            }
             return culture;
         }
 
